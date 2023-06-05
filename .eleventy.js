15a46f3b21c34539fe80ce84843e344ab48859b5
Michael <michaelmannucci@live.com>
2023-06-05 09:34:15 -0400

    configure image optimization

--- .eleventy.js
+++ .eleventy.js
@@ -16,52 +16,50 @@ module.exports = function (eleventyConfig) {
     "./node_modules/alpinejs/dist/cdn.min.js": "./js/alpine.js",
     "./node_modules/@alpinejs/focus/dist/cdn.min.js": "./js/alpine-focus.js",
     "./node_modules/lottie-web/build/player/lottie.min.js": "./js/lottie.js",
-  });
+  })
 
-  // Copy hamburger.json to /_Site
+  // Copy various files to /_Site
   eleventyConfig.addPassthroughCopy({
     "./src/hamburger.json": "./hamburger.json",
-  });
+    "./src/img/svg/*": "./img/svg/"
+  })
 
-
-
-
-  function imageShortcode(src, alt, sizes = "(min-width: 1024px) 100vw, 50vw", className = "") {
-    let fullSrc = path.join(__dirname, "/src/img/", src)
+  // Images plugin
+  function imageShortcode(src, alt, classes = "", sizes = "(max-width: 1536px) 100vw, 1536px") {
+    fullSrc = path.join(__dirname, "/src/img/", src)
     console.log(`Generating image(s) from:  ${fullSrc}`)
-    let options = {
-      widths: [600, 900, 1500],
-      formats: ["webp", "jpeg"],
+    const options = {
+      widths: [480, 768, 1200],
+      formats: ["webp", "jpeg", "svg"],
       urlPath: "/img/",
       outputDir: "./_site/img/",
       filenameFormat: function (id, fullSrc, width, format, options) {
         const extension = path.extname(fullSrc)
         const name = path.basename(fullSrc, extension)
         return `${name}-${width}w.${format}`
-      }
+      },
+      svgShortCircuit: true,
+      svgAllowUpscale: false
     }
-  
-    // generate images
+
+    // Generate images
     Image(fullSrc, options)
-  
-    let imageAttributes = {
+
+    const imageAttributes = {
       alt,
       sizes,
       loading: "lazy",
       decoding: "async",
-      class: className // add the class parameter to the imageAttributes object
+      class: classes
     }
-    // get metadata
+
+    // Get metadata
     metadata = Image.statsSync(fullSrc, options)
     return Image.generateHTML(metadata, imageAttributes)
   }
+
   eleventyConfig.addShortcode("image", imageShortcode)
-  // --- END, eleventy-img
-
-
-
-
-
+  // Images plugin end
 
   // Transform HTML as njk
   return {
@@ -70,6 +68,4 @@ module.exports = function (eleventyConfig) {
     },
     htmlTemplateEngine: "njk",
   }
-
-
 }
